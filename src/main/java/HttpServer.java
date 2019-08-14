89bdc880c34ae69599c86ca9bb0e034f7a8ebb4b
Tola's Terminal <sohbaker@gmail.com>
2019-08-14 09:08:59 +0100

    receives a get request. passes output stream to server constructor

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -1,17 +1,49 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 
 public class HttpServer {
     private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private BufferedReader clientInput;
+    private PrintWriter output;
 
-    public HttpServer(ServerSocket serverSocket) {
+    public HttpServer(ServerSocket serverSocket, PrintWriter output) {
         this.serverSocket = serverSocket;
+        this.output = output;
     }
 
     public void listen() {
         try {
-            Socket clientSocket = serverSocket.accept();
-            System.out.println(clientSocket);
+            clientSocket = serverSocket.accept();
+            setUpIOStreams();
+        } catch (IOException ex) {
+            output.println(ex);
+        }
+    }
+
+    public String parseInput() {
+        String request = "";
+        try {
+            request = clientInput.readLine();
+        } catch (IOException ex) {
+            output.println(ex);
+        }
+        return request;
+    }
+
+    private void setUpIOStreams() {
+        try {
+            clientInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter clientOutput = new PrintWriter(clientSocket.getOutputStream(), true);
+            output.println("IO streams created");
+        } catch (IOException ex) {
+            output.println(ex);
+        }
+    }
+
+    public void stop() {
+        try {
+            serverSocket.close();
         } catch (IOException ex) {
             System.out.println(ex);
         }
--- src/test/java/HttpServerTest.java
+++ src/test/java/HttpServerTest.java
@@ -2,25 +2,39 @@ import org.junit.*;
 import java.io.*;
 import java.net.*;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class HttpServerTest {
-        private MockServerSocket mockServerSocket;
-        private HttpServer server;
-        
-        @Before
-        public void setUpServer() throws IOException {
-             String fakeInput = "GET / HTTP/1.1";
-             ByteArrayInputStream clientInput = new ByteArrayInputStream((fakeInput).getBytes());
-             ByteArrayOutputStream clientOutput = new ByteArrayOutputStream();
-             Socket mockClientSocket = new MockClientSocket(clientInput, clientOutput);
-             mockServerSocket = new MockServerSocket(mockClientSocket);
-             server = new HttpServer(mockServerSocket);
-        }
+    private MockServerSocket mockServerSocket;
+    private HttpServer server;
+    private String fakeInput = "GET / HTTP/1.1";
+    private PrintWriter serverMessages = new PrintWriter(new StringWriter(), true);
 
-        @Test
-        public void acceptsANewClientConnection() {
-            server.listen();
-            assertTrue(mockServerSocket.wasAcceptCalled());
-        }
+    @Before
+    public void setUpServer() throws IOException {
+        ByteArrayInputStream clientInput = new ByteArrayInputStream((fakeInput).getBytes());
+        ByteArrayOutputStream clientOutput = new ByteArrayOutputStream();
+        Socket mockClientSocket = new MockClientSocket(clientInput, clientOutput);
+        mockServerSocket = new MockServerSocket(mockClientSocket);
+        server = new HttpServer(mockServerSocket, serverMessages);
+    }
+
+    @After
+    public void closeServer() {
+        server.stop();
+    }
+
+    @Test
+    public void acceptsANewClientConnection() {
+        server.listen();
+        assertTrue(mockServerSocket.wasAcceptCalled());
+    }
+
+    @Test
+    public void receivesGetRequest() {
+        server.listen();
+        String request = server.parseInput();
+        assertEquals(fakeInput, request);
+    }
 }
