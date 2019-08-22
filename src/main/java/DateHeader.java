90906dfd64b0345a1d069f40e9cab1d4abc78ac7
Tola's Terminal <sohbaker@gmail.com>
2019-08-22 23:17:26 +0100

    creates a date header

---
+++ src/main/java/DateHeader.java
@@ -0,0 +1,12 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateHeader {
+    public String getDate() {
+        LocalDateTime now = LocalDateTime.now();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-LLL-yyyy HH:mm:ss");
+
+        return now.format(formatter);
+    }
+}
