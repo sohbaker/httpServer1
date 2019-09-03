369b0ebfe3e5a5334ba5edb5793edfc6a2d957a4
Tola's Terminal <sohbaker@gmail.com>
2019-09-03 11:56:57 +0100

    router recieves a request when initialised

--- src/main/java/server/router/Router.java
+++ src/main/java/server/router/Router.java
@@ -1,4 +1,11 @@
 package server.router;
 
+import server.request.Request;
+
 public class Router {
+    Request request;
+
+    public Router(Request request) {
+        this.request = request;
+    }
 }
