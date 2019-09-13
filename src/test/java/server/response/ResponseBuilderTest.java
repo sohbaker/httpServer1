e248e88377ea5520ccd28d36b34109294b64ac11
Tola's Terminal <sohbaker@gmail.com>
2019-09-13 17:51:59 +0100

    adds and amends tests

--- src/test/java/server/response/ResponseBuilderTest.java
+++ src/test/java/server/response/ResponseBuilderTest.java
@@ -11,6 +11,6 @@ public class ResponseBuilderTest {
         ResponseBuilder responseBuilder = new ResponseBuilder();
 
         Response response = responseBuilder.build(StatusCode._200, null, null, "body");
-        assertEquals(response.toString(), true, containsString("body"));
+        assertThat(response.toString(), containsString("body"));
     }
 }
--- src/test/java/server/response/ResponseTest.java
+++ src/test/java/server/response/ResponseTest.java
@@ -1,7 +1,14 @@
 package server.response;
 
+import org.junit.*;
+
 import static org.junit.Assert.*;
 
 public class ResponseTest {
-
+    @Test
+    public void returnsAResponseStringSetWithGivenValues() {
+        Response response = new Response();
+        Response result = response.setStatusLine(StatusCode._200).setHeaders("Test", "this is a header").setBody("hello");
+        assertEquals("HTTP/1.1 200 OK\r\nTest: this is a header\r\n\r\nhello", result.toString());
+    }
 }
