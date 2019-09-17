dbeaf0b787475aa96ba7a5cde18e6495ac4130b8
Tola's Terminal <sohbaker@gmail.com>
2019-09-18 00:01:59 +0100

    allows user defined port or default

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -4,10 +4,21 @@ import java.io.*;
 import java.net.ServerSocket;
 
 public class Main {
+    private static int port;
+
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(5000);
+        getPort(args);
+        ServerSocket serverSocket = new ServerSocket(port);
         HttpServer server = new HttpServer(serverSocket, new PrintWriter(System.out, true));
 
         server.start();
+    }
+
+    private static void getPort(String[] args) {
+        if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 5000;
+        }
     }
 }
