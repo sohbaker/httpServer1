08cc4b94b432d0388a0a6e972d30d8414b10326e
Tola's Terminal <sohbaker@gmail.com>
2019-09-17 22:10:16 +0100

    adds handler mock

--- src/test/java/server/routing/MockRequestHandler.java
+++ src/test/java/server/routing/MockRequestHandler.java
@@ -1,4 +1,17 @@
 package server.routing;
 
-public class MockRequestHandler {
+import server.request.Request;
+import server.response.Response;
+
+public class MockRequestHandler implements RequestHandler {
+    private Response response;
+
+    public MockRequestHandler(Response response) {
+        this.response = response;
+    }
+
+    @Override
+    public Response handle(Request request) {
+        return this.response;
+    }
 }
