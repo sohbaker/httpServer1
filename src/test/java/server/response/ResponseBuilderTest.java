82560b2d9eeb9e3a76867a7335103bf980455ed5
Tola's Terminal <sohbaker@gmail.com>
2019-09-18 00:04:06 +0100

    amends test

--- src/test/java/server/response/ResponseBuilderTest.java
+++ src/test/java/server/response/ResponseBuilderTest.java
@@ -2,7 +2,6 @@ package server.response;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 
 public class ResponseBuilderTest {
@@ -10,7 +9,7 @@ public class ResponseBuilderTest {
     public void returnsAResponse() {
         ResponseBuilder responseBuilder = new ResponseBuilder();
 
-        Response response = responseBuilder.build(StatusCode._200, null, null, "body");
-        assertThat(response.toString(), containsString("body"));
+        Response response = responseBuilder.build(StatusCode._200, "Test", "has a header", "and body");
+        assertEquals("HTTP/1.1 200 OK\r\nTest: has a header\r\n\r\nand body", response.toString());
     }
 }
